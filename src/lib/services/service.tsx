d50254b4e0f1cf0850b5eee2feabf73e393cd777
Spatovaliyski <martinspatovaliyski@gmail.com>
2024-02-04 00:26:20 +0200

    Modify some line endings to preserve consistency

--- src/lib/services/service.tsx
+++ src/lib/services/service.tsx
@@ -1,7 +1,6 @@
 import { API_ROOT } from "../config/config";
 import { httpClient } from "./client";
 
-
 /**
  * Main service for the application.
  * 
@@ -14,9 +13,9 @@ import { httpClient } from "./client";
  * service.getItems();
  */
 const service = {
-	getItems: function () {
-		return httpClient.get(`${API_ROOT}`)
-	}
+  getItems: function () {
+    return httpClient.get(`${API_ROOT}`)
+  }
 };
 
 export default service;
